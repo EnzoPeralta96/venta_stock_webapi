51939a7f53392f5df395eb2e765e5386ab94873d
Juan Ruiz <juanignacioruizr@gmail.com>
2025-09-23 22:05:59 -0300

    me pide commit antes de merge con location

--- obj/Debug/net8.0/proyecto_venta_stock.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyecto_venta_stock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2534ceb30586db4446a60c268302f06a78b1a046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6942e629dbc8b9f4997374353d5d6b1a0b02725")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
