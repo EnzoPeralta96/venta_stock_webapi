698e958e6d4b920b5d1735839432ff7141976803
EnzoPeralta96 <enzash.k@gmail.com>
2025-11-21 17:22:24 -0300

    Commit para switch

--- obj/Debug/net8.0/proyecto_venta_stock.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyecto_venta_stock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22df0afe1670914e1d86f9b42d3fe02a704b83f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ab604289339165d5a3c13bac85ad376069c833")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_venta_stock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
